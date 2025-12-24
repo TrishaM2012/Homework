e0a3060a5290b5b8e682ab375057df3a76bb753b
TrishaM2012 <Sandy.avaya@gmail.com>
2025-12-24 13:37:42 +0000

    inital commit

---
+++ Random Password Challenge.py
@@ -0,0 +1,10 @@
+import random
+
+print("Your New Password is:")
+for i in range(4):
+    num = str(print(random.randint(1,20)))
+    letter = str(print(random.choice("AbCdEfGhIjKlMnOpQrStUvWxYz")))
+
+    
+
+     
